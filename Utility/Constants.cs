3d656a608df258b257c897854a34f4ce41ca9c1f
Allan-Moonjelil01 <allan.moonjelil01@whitireianz.ac.nz>
2025-04-16 16:49:58 +1200

    Add project files.

---
+++ Utility/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    /// 
+    /// Constants used in entire project defined in this class
+    /// 
+    public static class Constants
+    {
+    }
+}
