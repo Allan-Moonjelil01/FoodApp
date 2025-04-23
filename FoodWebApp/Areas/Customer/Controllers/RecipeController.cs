4093668a6e298c11c94a393148868856a149bdd0
samnetnz <sam.net.nz@gmail.com>
2025-04-23 07:39:48 +0530

    added header, carousel , footer added recipes, search bar in to home page

---
+++ FoodWebApp/Areas/Customer/Controllers/RecipeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourAppName.Areas.Customer.Controllers
+{
+	[Area("Customer")]
+	public class RecipeController : Controller
+	{
+		public IActionResult RecipeIndex()
+		{
+			return View();
+		}
+	}
+}
