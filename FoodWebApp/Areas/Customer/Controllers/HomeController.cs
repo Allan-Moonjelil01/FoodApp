fc3c3c73780d037fc34bd63f862304b8da6459fb
Allan-Moonjelil01 <allan.moonjelil01@whitireianz.ac.nz>
2025-05-23 17:59:38 +1200

    Homepage code refactored

--- FoodWebApp/Areas/Customer/Controllers/HomeController.cs
+++ FoodWebApp/Areas/Customer/Controllers/HomeController.cs
@@ -11,15 +11,25 @@ namespace FoodWebApp.Areas.Customer.Controllers;
 [Area("Customer")]
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly IWebHostEnvironment _env;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(IWebHostEnvironment env)
     {
-        _logger = logger;
+        _env = env;
     }
 
     public IActionResult Index()
     {
+        // Path to the images folder
+        var imageFolder = Path.Combine(_env.WebRootPath, "images\\carouselImages");
+        // Get all image file paths (can add more extensions if needed)
+        var imageFiles = Directory
+            .GetFiles(imageFolder, "*.*")
+            .Where(file => file.EndsWith(".jpg") || file.EndsWith(".png") || file.EndsWith(".jpeg"))
+            .Select(file => "/images/carouselImages/" + Path.GetFileName(file))
+            .ToList();
+
+        ViewBag.ImagePaths = imageFiles;
         return View();
     }
 
